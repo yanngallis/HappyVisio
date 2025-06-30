e28d60e7dd97df15b7392d9adc1a1362f18f407d
Yann Gallis <yann@gallis.me>
2025-06-30 10:34:49 +0200

    [#16] - Correction d'une traduction dans les tests

--- tests/LoginControllerTest.php
+++ tests/LoginControllerTest.php
@@ -62,7 +62,7 @@ class LoginControllerTest extends WebTestCase
         $this->client->followRedirect();
 
         // Ensure we do not reveal if the user exists or not.
-        self::assertSelectorTextContains('.alert-danger', 'Invalid credentials.');
+        self::assertSelectorTextContains('.alert-danger', 'Identifiants invalides.');
 
         // Denied - Can't login with invalid password.
         $this->client->request('GET', '/login');
@@ -77,7 +77,7 @@ class LoginControllerTest extends WebTestCase
         $this->client->followRedirect();
 
         // Ensure we do not reveal the user exists but the password is wrong.
-        self::assertSelectorTextContains('.alert-danger', 'Invalid credentials.');
+        self::assertSelectorTextContains('.alert-danger', 'Identifiants invalides.');
 
         // Success - Login with valid credentials is allowed.
         $this->client->submitForm('Sign in', [
